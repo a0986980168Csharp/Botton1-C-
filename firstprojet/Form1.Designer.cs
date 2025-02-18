0bf0e1cc992f76d956e73cbe4d34a324f36b3792
a0986980168Csharp <a0986980168@saihs.edu.tw>
2025-02-18 15:55:35 +0800

    firstcommit

---
+++ firstprojet/Form1.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace firstprojet
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            button1 = new Button();
+            textBox1 = new TextBox();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Font = new Font("Microsoft JhengHei UI", 26.25F, FontStyle.Bold, GraphicsUnit.Point);
+            button1.ForeColor = SystemColors.Highlight;
+            button1.Location = new Point(90, 231);
+            button1.Margin = new Padding(6, 5, 6, 5);
+            button1.Name = "button1";
+            button1.Size = new Size(375, 130);
+            button1.TabIndex = 0;
+            button1.Text = "按我";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(93, 137);
+            textBox1.Margin = new Padding(6, 5, 6, 5);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(372, 34);
+            textBox1.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(13F, 26F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(540, 437);
+            Controls.Add(textBox1);
+            Controls.Add(button1);
+            Font = new Font("Microsoft JhengHei UI", 15.75F, FontStyle.Bold, GraphicsUnit.Point);
+            Margin = new Padding(6, 5, 6, 5);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button button1;
+        private TextBox textBox1;
+    }
+}
